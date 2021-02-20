c7e9dd96d64791b4550ffac17e491265b902127a
AndrewKrachko <AndrewKrachko@gmail.com>
2021-02-20 11:15:46 +0300

    Action for TemplateExample

--- ProgrammingPatterns/TemplateExample.cs
+++ ProgrammingPatterns/TemplateExample.cs
@@ -6,5 +6,29 @@ namespace ProgrammingPatterns
 {
     public class TemplateExample
     {
+        public abstract class EnterSaloonAbstractBehaviour
+        {
+            public void EnterSaloon()
+            {
+                OpenDoor();
+                ActionA();
+                WalkIn();
+                ActionB();
+                SitDown();
+                HookA();
+            }
+
+            protected void OpenDoor() => Console.WriteLine("Just opening the door");
+
+            protected void WalkIn() => Console.WriteLine("Making several steps");
+
+            protected void SitDown() => Console.WriteLine("Taking seat");
+
+            protected abstract void ActionA();
+            protected abstract void ActionB();
+            protected abstract void ActionC();
+
+            protected virtual void HookA() { }
+        }
     }
 }
